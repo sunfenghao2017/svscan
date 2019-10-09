64630cc40d3ec5c805aa929175f08d5b2c5fddeb
wood <wljlinksly@gmail.com>
2019-10-09 16:09:08 +0800

    Revert "bam_aux_append should be used in the first time to add an tag"
    Bug got, kill it and revert
    This reverts commit 951b34ad66a5fa6ac789698008e32a09f933dfe6.

--- src/stats.cpp
+++ src/stats.cpp
@@ -191,8 +191,7 @@ void Stats::stat(const SVSet& svs, const std::vector<std::vector<CovRecord>>& co
                                     }
                                     if(mOpt->fbamout){
                                         mOpt->outMtx.lock();
-                                        std::string idval = std::to_string(itbp->mID);
-                                        bam_aux_append(b, "ZF", 'i', idval.length() + 1, (uint8_t*)idval.c_str());
+                                        bam_aux_update_int(b, "ZF", itbp->mID);
                                         assert(sam_write1(mOpt->fbamout, h, b) >= 0);
                                         mOpt->outMtx.unlock();
                                     }
@@ -296,8 +295,7 @@ void Stats::stat(const SVSet& svs, const std::vector<std::vector<CovRecord>>& co
                             }
                             if(mOpt->fbamout){
                                 mOpt->outMtx.lock();
-                                std::string idval = std::to_string(itspan->mID);
-                                bam_aux_append(b, "ZF", 'i', idval.length() + 1, (uint8_t*)idval.c_str());
+                                bam_aux_update_int(b, "ZF", itspan->mID);
                                 assert(sam_write1(mOpt->fbamout, h, b) >= 0);
                                 mOpt->outMtx.unlock();
                             }
