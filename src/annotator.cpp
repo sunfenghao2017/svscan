923eb1f05d8aa736fb0a02ca69ec5401cd32215a
wood <wljlinksly@gmail.com>
2019-10-28 22:30:28 +0800

    rna transcript parser fixed

--- src/annotator.cpp
+++ src/annotator.cpp
@@ -341,10 +341,20 @@ void Annotator::geneAnnoRNA(SVSet& svs, GeneInfoList& gl){
         for(uint32_t g1 = 0; g1 < gl[i].mGene1.size(); ++g1){
             for(uint32_t g2 = 0; g2 < gl[i].mGene2.size(); ++g2){
                 FuseGene fsg = svutil::getFusionGene(gl[i].mGene1[g1].gene, gl[i].mGene2[g2].gene, '+', '+', svs[i].mSVT);
-                if(fsg.hgene == gl[i].mGene1[g1].gene) fsg.hidx = g1;
-                else fsg.hidx = g2;
-                if(fsg.tgene == gl[i].mGene1[g1].gene) fsg.tidx = g1;
-                else fsg.tidx = g2;
+                if(fsg.hgene == gl[i].mGene1[g1].gene){
+                    fsg.hidx = g1;
+                    fsg.hfrom1 = true;
+                }else{
+                    fsg.hidx = g2;
+                    fsg.hfrom1 = false;
+                }
+                if(fsg.tgene == gl[i].mGene1[g1].gene){
+                    fsg.tidx = g1;
+                    fsg.tfrom1 = true;
+                }else{ 
+                    fsg.tidx = g2;
+                    fsg.tfrom1 = false;
+                }
                 gl[i].mFuseGene.push_back(fsg);
             }
         }
