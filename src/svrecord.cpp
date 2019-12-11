c5f2d09a05f3ba3d6f69337c220f3d6617757b55
wood <wljlinksly@gmail.com>
2019-12-11 17:13:51 +0800

    fix sth

--- src/svrecord.cpp
+++ src/svrecord.cpp
@@ -266,7 +266,7 @@ void mergeSRSVs(SVSet& sr, SVSet& msr, Options* opt){
     }
     std::copy_if(sr.begin(), sr.end(), std::back_inserter(msr), [&](const SVRecord& sv){return !sv.mMerged;});
     util::loginfo(std::to_string(sr.size()) + " SR supported SVs merged into " + std::to_string(msr.size()) + " ones");
-    if(opt->debug){
+    if(opt->debug & DEBUG_FCALL){
         std::cout << "debug_Merged_SR_SV_IDs:";
         for(uint32_t i = 0; i < sr.size(); ++i){
             if(sr[i].mMerged){
@@ -328,7 +328,7 @@ void mergeDPSVs(SVSet& dp, SVSet& mdp, Options* opt){
     }
     std::copy_if(dp.begin(), dp.end(), std::back_inserter(mdp), [&](const SVRecord& sv){return !sv.mMerged;});
     util::loginfo(std::to_string(dp.size()) + " DP supported SVs merged into " + std::to_string(mdp.size()) + " ones");
-    if(opt->debug){
+    if(opt->debug & DEBUG_FCALL){
         std::cout << "debug_Merged_DP_SV_IDs:";
         for(uint32_t i = 0; i < dp.size(); ++i){
             if(dp[i].mMerged){
