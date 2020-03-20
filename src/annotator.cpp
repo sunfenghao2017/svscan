71ef496ae2adf0ba0c792e2cb0cc24d5fae3d292
vanNul <wljlinksly@icloud.com>
2020-03-20 17:34:54 +0800

    another try

--- src/annotator.cpp
+++ src/annotator.cpp
@@ -435,10 +435,67 @@ void Annotator::refineCovAnno(Stats* sts, const SVSet& svs){
     // compute realnThreshold
     int32_t realnThreshold = std::max(mOpt->fuseOpt->mWhiteFilter.mMaxRepHit, mOpt->fuseOpt->mUsualFilter.mMaxRepHit) + 1;
     ReadSupportStatMap rssm;
+    std::set<int32_t> nosrssvs;
     getReadSupportStatus(mOpt->bamout, rssm, mOpt->realnf);
     std::map<std::string, int32_t> drec;
     for(auto iter = rssm.begin(); iter != rssm.end(); ++iter){
-        if(iter->second->mR1Hit > 2) svs[iter->second->mR1SVID]->mRealnRet = realnThreshold;
+        bool r1rpt = false , r2rpt = false;
+        // check r1 repeat region
+        if(iter->second->mR1Hit > 2){
+            // check r1
+            if(iter->second->mR1PHit > 1){
+                if(!mOpt->overlapRegs->overlap(iter->second->mR1PChr.c_str(), iter->second->mR1PBeg, iter->second->mR1PEnd)){// not in probe region
+                    r1rpt = true;
+                }
+            }
+            if(!r1rpt){
+                if(iter->second->mR1SHit > 1){
+                    if(!mOpt->overlapRegs->overlap(iter->second->mR1SChr.c_str(), iter->second->mR1SBeg, iter->second->mR1SEnd)){// not in probe region
+                        r1rpt = true;
+                    }
+                }
+            }
+            if(r1rpt){
+                sts->mTotalAltCnts[iter->second->mR1SVID] -= 1;
+                nosrssvs.insert(iter->second->mR1SVID);
+                if(iter->second->mR1SRT){
+                     sts->mSpnCnts[iter->second->mR1SVID].mAltCnt -= 1;
+                     sts->mSpnCnts[iter->second->mR1SVID].mAltQual[iter->second->mR1MapQ] -= 1;
+                 }else{
+                     sts->mJctCnts[iter->second->mR1SVID].mAltCnt -= 1;
+                     sts->mJctCnts[iter->second->mR1SVID].mAltQual[iter->second->mR1MapQ] -= 1;
+                 }
+                 drec[iter->first] = 3;
+            }
+        }
+        if(iter->second->mR2Hit > 2){
+            // check r2
+            if(iter->second->mR2PHit > 1){
+                if(!mOpt->overlapRegs->overlap(iter->second->mR2PChr.c_str(), iter->second->mR2PBeg, iter->second->mR2PEnd)){// not in probe region
+                    r2rpt = true;
+                }
+            }
+            if(!r2rpt){
+                if(iter->second->mR2SHit > 1){
+                    if(!mOpt->overlapRegs->overlap(iter->second->mR2SChr.c_str(), iter->second->mR2SBeg, iter->second->mR2SEnd)){// not in probe region
+                        r2rpt = true;
+                    }
+                }
+            }
+            if(r2rpt){
+                sts->mTotalAltCnts[iter->second->mR2SVID] -= 1;
+                nosrssvs.insert(iter->second->mR2SVID);
+                if(iter->second->mR2SRT){
+                     sts->mSpnCnts[iter->second->mR2SVID].mAltCnt -= 1;
+                     sts->mSpnCnts[iter->second->mR2SVID].mAltQual[iter->second->mR2MapQ] -= 1;
+                 }else{
+                     sts->mJctCnts[iter->second->mR2SVID].mAltCnt -= 1;
+                     sts->mJctCnts[iter->second->mR2SVID].mAltQual[iter->second->mR2MapQ] -= 1;
+                 }
+                 drec[iter->first] = 3;
+            }
+        }
+        if(r1rpt || r2rpt) continue;
         if(iter->second->mR2Hit > 2) svs[iter->second->mR2SVID]->mRealnRet = realnThreshold;
         if(iter->second->mR1MapQ && iter->second->mR2MapQ){
             if(iter->second->mR1SVID != iter->second->mR2SVID){
@@ -491,6 +548,23 @@ void Annotator::refineCovAnno(Stats* sts, const SVSet& svs){
                 sts->mTotalAltCnts[iter->second->mR1SVID] -= 1;
             }
         }
+    }
+    // second run, kick out all the rescued sr with seed cleared
+    for(auto iter = rssm.begin(); iter != rssm.end(); ++iter){
+        if(nosrssvs.find(iter->second->mR1SVID) != nosrssvs.end() ||
+           nosrssvs.find(iter->second->mR2SVID) != nosrssvs.end()){
+            drec[iter->first] = 3;
+        }
+    }
+    // clear invalid svs
+    for(auto& id: nosrssvs){
+        sts->mSpnCnts[id].mAltCnt = 0;
+        sts->mSpnCnts[id].mAltQual.clear();
+        sts->mJctCnts[id].mAltCnt = 0;
+        sts->mJctCnts[id].mAltQual.clear();
+        svs[id]->mSRSupport = 0;
+        svs[id]->mPESupport = 0;
+        svs[id]->mRealnRet = -4;
     }
     // write to result
     samFile* ifp = sam_open(mOpt->bamout.c_str(), "r");
--- src/resbamstat.h
+++ src/resbamstat.h
@@ -11,12 +11,29 @@
 struct ReadSupport{
     int32_t mR1SVID = -1;
     uint8_t mR1MapQ = 0;
-    int32_t mR1Hit = 0;
     int8_t mR1SRT = -1;
+    int32_t mR1Hit = 0;
+    int32_t mR1PHit = 0;
+    std::string mR1PChr;
+    int32_t mR1PBeg = -1;
+    int32_t mR1PEnd = -1;
+    int32_t mR1SHit = 0;
+    std::string mR1SChr;
+    int32_t mR1SBeg = -1;
+    int32_t mR1SEnd = -1;
+    
     int32_t mR2SVID = -1;
     uint8_t mR2MapQ = 0;
     int8_t mR2SRT = -1;
     int32_t mR2Hit = 0;
+    int32_t mR2PHit = 0;
+    std::string mR2PChr;
+    int32_t mR2PBeg = -1;
+    int32_t mR2PEnd = -1;
+    int32_t mR2SHit = 0;
+    std::string mR2SChr;
+    int32_t mR2SBeg = -1;
+    int32_t mR2SEnd = -1;
 };
 
 /** type to store read supporting statistics */
@@ -30,7 +47,8 @@ inline void getReadSupportStatus(const std::string& bam, ReadSupportStatMap& rss
         uint8_t* idata = bam_aux_get(b, "ZF");
         uint8_t* sdata = bam_aux_get(b, "ST");
         uint8_t* saval = bam_aux_get(b, "SA");
-        int32_t lhit = 0, thit = 0;
+        int32_t lhit = 0, lbeg = -1, lend = -1, thit = 0, tbeg = -1, tend = -1;
+        std::string lchr, tchr;
         if(saval){
             int32_t sclen = 0;
             for(uint32_t i = 0; i < b->core.n_cigar; ++i){
@@ -39,6 +57,9 @@ inline void getReadSupportStatus(const std::string& bam, ReadSupportStatMap& rss
                     break;
                 }
             }
+            lchr = h->target_name[b->core.tid];
+            lbeg = b->core.pos;
+            lend = bam_endpos(b);
             std::string qseq(b->core.l_qseq, '\0');
             for(int32_t i = 0; i < b->core.l_qseq; ++i){
                 qseq[i] = seq_nt16_str[bam_seqi(bam_get_seq(b), i)];
@@ -47,6 +68,14 @@ inline void getReadSupportStatus(const std::string& bam, ReadSupportStatMap& rss
             std::string tseq = qseq.substr(sclen);
             lhit = rf->validSRSeq(lseq);
             thit = rf->validSRSeq(tseq);
+            std::string ss = bam_aux2Z(saval);
+            std::vector<std::string> vsas;
+            util::split(ss, vsas, ";");
+            std::vector<std::string> vstr;
+            util::split(vsas[0], vstr, ",");
+            tchr = vstr[0];
+            tbeg = std::atoi(vstr[1].c_str());
+            tend = tbeg + sclen;
         }
         if(idata && sdata){
             int svid = bam_aux2i(idata);
@@ -60,11 +89,30 @@ inline void getReadSupportStatus(const std::string& bam, ReadSupportStatMap& rss
                     rs->mR1MapQ = b->core.qual;
                     rs->mR1SRT = srst;
                     rs->mR1Hit = lhit + thit;
+                    if(saval){
+                        rs->mR1PHit = lhit;
+                        rs->mR1SHit = thit;
+                        rs->mR1PChr = lchr;
+                        rs->mR1SChr = tchr;
+                        rs->mR1PBeg = lbeg;
+                        rs->mR1PEnd = lend;
+                        rs->mR1SBeg = tbeg;
+                        rs->mR1SEnd = tend;
+                    }
                 }else{
                     rs->mR2SVID = svid;
                     rs->mR2MapQ = b->core.qual;
                     rs->mR2SRT = srst;
                     rs->mR2Hit = lhit + thit;
+                    if(saval){
+                        rs->mR2PHit = lhit;
+                        rs->mR2SHit = thit;
+                        rs->mR2PChr = lchr;
+                        rs->mR2SChr = tchr;
+                        rs->mR2PBeg = lbeg;
+                        rs->mR2PEnd = lend;
+                        rs->mR2SBeg = tbeg;
+                    }
                 }
                 rssm[qname] = rs;
             }else{
@@ -73,11 +121,31 @@ inline void getReadSupportStatus(const std::string& bam, ReadSupportStatMap& rss
                     iter->second->mR1MapQ = b->core.qual;
                     iter->second->mR1SRT = srst;
                     iter->second->mR1Hit = lhit + thit;
+                    if(saval){
+                        iter->second->mR1PHit = lhit;
+                        iter->second->mR1SHit = thit;
+                        iter->second->mR1PChr = lchr;
+                        iter->second->mR1SChr = tchr;
+                        iter->second->mR1PBeg = lbeg;
+                        iter->second->mR1PEnd = lend;
+                        iter->second->mR1SBeg = tbeg;
+                        iter->second->mR1SEnd = tend;
+                    }
                 }else{
                     iter->second->mR2SVID = svid;
                     iter->second->mR2MapQ = b->core.qual;
                     iter->second->mR2SRT = srst;
                     iter->second->mR2Hit = lhit + thit;
+                    if(saval){
+                        iter->second->mR2PHit = lhit;
+                        iter->second->mR2SHit = thit;
+                        iter->second->mR2PChr = lchr;
+                        iter->second->mR2SChr = tchr;
+                        iter->second->mR2PBeg = lbeg;
+                        iter->second->mR2PEnd = lend;
+                        iter->second->mR2SBeg = tbeg;
+                        iter->second->mR2SEnd = tend;
+                    }
                 }
             }
         }
