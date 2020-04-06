5f4b965caad1465610f71ec9a689cb27b6d98a28
vanNul <wljlinksly@icloud.com>
2020-04-06 11:38:26 +0800

    output alternative realn bam in svbam submodule

--- src/svbam.cpp
+++ src/svbam.cpp
@@ -2,23 +2,48 @@
 
 void SVBAMOpt::getBam(){
     std::string tobam = obam + ".tmp.unsorted.bam";
+    std::string tobam2 = obam2 + ".tmp.unsorted.bam";
     const char* idt = "ZF";
     samFile* ifp = sam_open(ibam.c_str(), "r");
     samFile* ofp = sam_open(tobam.c_str(), "w");
+    samFile* ofp2 = NULL;
     bam_hdr_t* h = sam_hdr_read(ifp);
     assert(sam_hdr_write(ofp, h) >= 0);
+    if(outalt){
+        ofp2 = sam_open(tobam2.c_str(), "w");
+        assert(sam_hdr_write(ofp2, h) >= 0);
+    }
     bam1_t* b = bam_init1();
+    std::vector<bam1_t*> realn;
     while(sam_read1(ifp, h, b) >= 0){
         if(bam_aux2i(bam_aux_get(b, idt)) == svid){
             assert(sam_write1(ofp, h, b) >= 0);
         }
+        if(outalt){
+            UnalignedSeq us;
+            us.mName = bamutil::getQName(b);
+            us.mSeq = bamutil::getSeq(b);
+            realn.clear();
+            obwa->alignSeq(us, realn);
+            for(auto& e: realn){
+                assert(sam_write1(ofp2, h, e) >= 0);
+                bam_destroy1(e);
+            }
+        }
     }
     sam_close(ifp);
     sam_close(ofp);
+    if(outalt) sam_close(ofp2);
     bam_hdr_destroy(h);
     bam_destroy1(b);
     std::string sortCMD = "samtools sort -o " + obam + " " + tobam;
     system(sortCMD.c_str());
     remove(tobam.c_str());
+    if(outalt){
+        sortCMD = "samtools sort -o " + obam2 + " " + tobam2;
+        system(sortCMD.c_str());
+        remove(tobam2.c_str());
+        assert(sam_index_build(obam2.c_str(), 14) == 0);
+    }
     assert(sam_index_build(obam.c_str(), 14) == 0);
 }
--- src/svbam.h
+++ src/svbam.h
@@ -7,23 +7,34 @@
 #include <vector>
 #include <htslib/sam.h>
 #include "software.h"
+#include "onlinebwa.h"
 #include <util.h>
+#include <bamutil.h>
 
 struct SVBAMOpt{
     std::string ibam;
     int32_t svid;
     std::string obam;
+    std::string obam2;
+    std::string ref;
+    bool outalt;
     Software* softEnv;
+    OnlineBWA* obwa;
     
     SVBAMOpt(){
         softEnv = new Software();
         softEnv->cmp += "version: " + softEnv->ver + "\n";
         softEnv->cmp += "updated: " + std::string(__TIME__) + " " + std::string(__DATE__);
         obam = "./svi.bam";
+        obam2 = "";
+        ref = "";
+        outalt = false;
+        obwa = NULL;
     }
 
     ~SVBAMOpt(){
         if(softEnv) delete softEnv;
+        if(obwa) delete obwa;
     }
 
     void update(int argc, char** argv){
@@ -31,6 +42,11 @@ struct SVBAMOpt{
         for(int i = 0; i < argc; ++i){
             softEnv->cmd.append(argv[i]);
             softEnv->cmd.append(" ");
+        }
+        if(obam2.size() && ref.size()){
+            outalt = true;
+            obwa = new OnlineBWA();
+            obwa->loadIndex(ref);
         }
     }
 
